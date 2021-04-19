151ea3a54111e9f6b30a7003c645ff17687986c7
Recep Aslantas <info@recp.me>
2021-04-20 00:40:58 +0300

    Create common.h

---
+++ include/phy/version.h
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the phy project (https://github.com/recp/phy)
+ * Copyright (c) Recep Aslantas.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef phy_version_h
+#define phy_version_h
+
+#define PHY_VERSION_MAJOR 0
+#define PHY_VERSION_MINOR 1
+#define PHY_VERSION_PATCH 0
+
+#endif /* phy_version_h */
